68dd954a224b66cf28b2825627399872bc3f4121
Darshana K <darshanak884@gmail.com>
2025-09-20 10:35:57 +0530

    hallo

---
+++ hallo-world.py
@@ -0,0 +1,10 @@
+from flask import Flask
+# Create Flask application
+app = Flask(__name__)
+# Define a route and view function
+@app.route('/') def
+hello():
+return "Hello, World!"
+# Run the application if
+__name__ == '__main__':
+app.run(debug=True)
